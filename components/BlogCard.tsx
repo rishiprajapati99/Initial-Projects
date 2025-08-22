61c703b3b586184d1850dea42e4ac22f308e2bfc
rishiprajapati99 <santoshprajapati52911@gmail.com>
2025-08-22 06:48:46 +0530

    Added buttons to the BlogCard

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -18,6 +18,7 @@ const Blogcard = () => {
         <Text style={[styles.headingText]}>Blogcard</Text>
       </View>
       <View style={[styles.ImageContainer]}>
+        <Text style={[styles.ImageTitle]}>What's New?</Text>
         <Image
           source={{
             uri: 'https://cdn.dribbble.com/userupload/14918082/file/original-680a48f7f8325b0f5893897665b5d900.jpg?resize=400x0',
@@ -26,22 +27,23 @@ const Blogcard = () => {
         />
 
         <View style={[styles.ImageText]}>
-          <Text style={[styles.ImageTitle]}>What's New?</Text>
           <Text>
             Blog cards serve as the gateway to your content — transforming
             simple article links into visually compelling previews that entice
             readers
           </Text>
-          <TouchableOpacity
-            onPress={() => openWebsite('https://dribbble.com/tags/blog-card')} //provided the callBack function (openWebsite)
-          >
-            <Text style={{ color: 'blue' }}>Read more</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => openWebsite('https://github.com/rishiprajapati99')}
-          >
-            <Text style={{ color: 'blue' }}>Follow Me</Text>
-          </TouchableOpacity>
+          <View style={[styles.footerLinks]}>
+            <TouchableOpacity
+              onPress={() => openWebsite('https://dribbble.com/tags/blog-card')} //provided the callBack function (openWebsite)
+            >
+              <Text style={[styles.socialLinks]}>Read more</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => openWebsite('https://github.com/rishiprajapati99')}
+            >
+              <Text style={[styles.socialLinks]}>Follow Me</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
@@ -66,7 +68,7 @@ const styles = StyleSheet.create({
   },
   ImageContainer: {
     marginHorizontal: 12,
-    backgroundColor: 'white',
+    backgroundColor: '#c3edf4ff',
     elevation: 8,
     borderRadius: 8,
   },
@@ -76,9 +78,25 @@ const styles = StyleSheet.create({
     //  elevation: 8,
   },
   ImageText: {
-    margin: 6,
+    margin: 10,
   },
-  ImageTitle: { fontSize: 20, fontWeight: 'bold' },
+  ImageTitle: { fontSize: 20, fontWeight: 'bold', marginHorizontal: 4 },
+  footerLinks: {
+    marginTop: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  socialLinks: {
+    color: 'blue',
+    fontSize:15,
+    backgroundColor:'#edc09dff',
+    padding:6,//padding(defination=>space inside the box(view or container(can be <Text>)) and the content(ex text))=>defines the space between content and the border of the box
+    borderRadius:15,
+    // marginHorizontal:6,//margin =>space between boxes(view) . margin(defination=<space outside of the box(View))
+    paddingHorizontal:26,
+    borderWidth:1,
+    borderColor:'#cbae4eff'
+  },
 });
 
 export default Blogcard;
